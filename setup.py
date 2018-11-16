e356a4c410438b09a69a9112dd975f7ccd859420
Philip Kershaw <philip.kershaw@stfc.ac.uk>
2018-11-16 16:56:41 +0000

    fixes to licensing and copyright

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 """Distribution Utilities setup program for CCI ESGF Search ops test Package
-
-Contrail Project
 """
 __author__ = "P J Kershaw"
 __date__ = "09/11/17"
-__copyright__ = "(C) 2017 Science and Technology Facilities Council"
+__copyright__ = "Copyright 2018 United Kingdom Research and Innovation"
 __license__ = """BSD - See LICENSE file in top-level directory"""
 __contact__ = "Philip.Kershaw@stfc.ac.uk"
 __revision__ = '$Id$'
@@ -34,6 +32,11 @@ setup(
     license =           __license__,
     test_suite =        '',
     packages =          find_packages(),
+    package_data={
+        'ceda/cci_odp_ops_tests': [
+            'LICENSE',
+        ],
+    },
     classifiers = [
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
@@ -48,7 +51,6 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 2',
         'Topic :: Security',
         'Topic :: Internet',
         'Topic :: Scientific/Engineering',
